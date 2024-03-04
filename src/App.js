6322959f1a9969a64e6f8563d420ef64c48e8421
bahasaracoglu <bahasaracoglu@gmail.com>
2024-03-05 00:14:53 +0300

    appBar, list and dialog added.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import ToDoList from "./components/ToDoList";
+import BottomAppBar from "./components/BottomAppBar";
+import { Container } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="sm">
+        <BottomAppBar />
+      </Container>
     </div>
   );
 }
