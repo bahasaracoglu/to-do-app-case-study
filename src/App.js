9d30ab2a2eab2a277086addf9e2523f40902d98b
bahasaracoglu <bahasaracoglu@gmail.com>
2024-03-05 02:53:50 +0300

    login & signup pages added

--- src/App.js
+++ src/App.js
@@ -6,13 +6,36 @@ import "@fontsource/roboto/700.css";
 import ToDoList from "./components/ToDoList";
 import BottomAppBar from "./components/BottomAppBar";
 import { Container } from "@mui/material";
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import SignUpPage from "./components/SignUpPage";
+import LoginPage from "./components/LoginPage";
 
 function App() {
+  const router = createBrowserRouter([
+    { path: "*", element: <Navigate to="/login" /> },
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+    {
+      path: "/sign-up",
+      element: <SignUpPage />,
+    },
+    {
+      path: "/todo-list",
+      element: <BottomAppBar />,
+    },
+  ]);
   return (
     <div className="App">
-      <Container maxWidth="sm">
+      <RouterProvider router={router} />
+      {/* <Container maxWidth="sm">
         <BottomAppBar />
-      </Container>
+      </Container> */}
     </div>
   );
 }
--- src/components/FormDialog.js
+++ src/components/FormDialog.js
@@ -6,8 +6,23 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import styled from "@emotion/styled";
+import { Grid } from "@mui/material";
+import ImageIcon from "@mui/icons-material/Image";
 
 export default function FormDialog({ open, handleClose }) {
+  const VisuallyHiddenInput = styled("input")({
+    clip: "rect(0 0 0 0)",
+    clipPath: "inset(50%)",
+    height: 1,
+    overflow: "hidden",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    whiteSpace: "nowrap",
+    width: 1,
+  });
   return (
     <React.Fragment>
       <Dialog
@@ -25,27 +40,50 @@ export default function FormDialog({ open, handleClose }) {
           },
         }}
       >
-        <DialogTitle>Subscribe</DialogTitle>
+        <DialogTitle>Add new todo</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here.
-            We will send updates occasionally.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            required
-            margin="dense"
-            id="name"
-            name="email"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                id="standard-basic"
+                label="Text"
+                variant="standard"
+                fullWidth="true"
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              {" "}
+              <Button
+                component="label"
+                role={undefined}
+                variant="contained"
+                tabIndex={-1}
+                startIcon={<CloudUploadIcon />}
+              >
+                Upload file
+                <VisuallyHiddenInput type="file" />
+              </Button>{" "}
+              <DialogContentText>File attachment</DialogContentText>
+            </Grid>{" "}
+            <Grid item xs={12} md={6}>
+              {" "}
+              <Button
+                component="label"
+                role={undefined}
+                variant="contained"
+                tabIndex={-1}
+                startIcon={<ImageIcon />}
+              >
+                Upload image
+                <VisuallyHiddenInput type="file" />
+              </Button>
+              <DialogContentText>Upload image for thumbnail</DialogContentText>
+            </Grid>
+          </Grid>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
-          <Button type="submit">Subscribe</Button>
+          <Button type="submit">Save</Button>
         </DialogActions>
       </Dialog>
     </React.Fragment>
